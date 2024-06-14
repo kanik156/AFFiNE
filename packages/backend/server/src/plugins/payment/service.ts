4ffb7bc9843f2f0ed21a4f22aa3e0baeaf1a1d37
Ken Taketani <taketani.ken.witchpot@gmail.com>
2024-06-15 00:57:22 +0900

    update

--- packages/backend/server/src/plugins/payment/service.ts
+++ packages/backend/server/src/plugins/payment/service.ts
@@ -155,6 +155,7 @@ export class SubscriptionService {
     redirectUrl: string;
     idempotencyKey: string;
   }) {
+    /*
     if (
       this.config.deploy &&
       this.config.affine.canary &&
@@ -162,6 +163,7 @@ export class SubscriptionService {
     ) {
       throw new BadRequestException('You are not allowed to do this.');
     }
+    */
 
     const currentSubscription = await this.db.userSubscription.findFirst({
       where: {
